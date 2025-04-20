414dae185c11813eb9f440048f232ca1fc54d426
HAEWON LEE <haewon09106@gmail.com>
2025-04-20 15:17:53 +0900

    fix:Image sizes 수정 lcp 향상용

--- src/features/products/components/ProductItem.tsx
+++ src/features/products/components/ProductItem.tsx
@@ -39,6 +39,11 @@ const ProductItem = ({
           alt={`${product.title} thumbnail`}
           width={150}
           height={150}
+          sizes={
+            viewMode === "list"
+              ? "(min-width: 1024px) 150px, 30vw"
+              : "(min-width: 1024px) 25vw, 50vw"
+          }
         />
       </div>
 
