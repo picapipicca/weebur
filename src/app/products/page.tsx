34b959f6f7f764acab8eb88709b587ad7bb046a8
HAEWON LEE <haewon09106@gmail.com>
2025-04-20 15:06:52 +0900

    fix:ssr  searchParam 수정

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,19 +8,25 @@ import ProductList from "@/features/products/components/ProductList";
 import ProductFilterForm from "@/features/products/components/ProductFilterForm";
 import { Spinner } from "@/shared/ui";
 
-export default async function ProductsPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
+export default async function ProductsPage(props: {
+  searchParams?: Promise<{
+    q?: string;
+    sortBy?: "rating" | "";
+    order?: "desc" | "";
+  }>;
 }) {
-  const { q, sortBy, order } = await searchParams;
+  // const { q, sortBy, order } = await searchParams;
+  const searchParams = await props.searchParams;
+  const q = searchParams?.q || "";
+  const sortBy = searchParams?.sortBy || "";
+  const order = searchParams?.order || undefined;
 
   const req: ProductsListRequest = {
     limit: 20,
     skip: 0,
-    q: typeof q === "string" ? q : "",
-    sortBy: sortBy === "rating" ? "rating" : "",
-    order: order === "desc" ? "desc" : undefined,
+    q,
+    sortBy,
+    order,
   };
 
   return (
